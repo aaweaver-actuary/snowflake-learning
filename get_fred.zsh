54b95a77315cb61b88b1fe44f7eb3ed895ccfa11
aaweaver-actuary <andrewayersweaver@gmail.com>
2024-04-02 15:09:44 +0000

    series yaml file

--- get_fred.zsh
+++ get_fred.zsh
@@ -2,6 +2,18 @@
 
 FOLDER=/home/andy/code/snowflake/
 
+SERIES=(
+    GDP
+    GDPC1
+    GDPDEF
+    UNRATE
+
+    # Auto Loan Rates
+    TERMCBAUTO48NS
+    RIFLPBCIANM60NM
+    RIFLPBCIANM72NM
+
+)
 
 python3 ${FOLDER}/fred.py get GDP
 python3 ${FOLDER}/fred.py get GDPDEF
